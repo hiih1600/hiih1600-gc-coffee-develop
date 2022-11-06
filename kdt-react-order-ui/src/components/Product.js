2b59d5523724a0321f44dc95ba87980b0b3591db
hiih1600 <yejin16000@gmail.com>
2022-11-06 17:47:59 +0900

    upload

---
+++ kdt-react-order-ui/src/components/Product.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export function Product(props) {
+    const productId = props.productId;
+    const productName = props.productName;
+    const category = props.category;
+    const price = props.price;
+
+    const handleAddBtnClicked = e => {
+        props.onAddClick(productId);
+    };
+    const handleAddBtnClicked2 = e => {
+        props.onSubstractClick(productId);
+    };
+
+
+    return <>
+        <div className="col-2">
+            <img className="img-fluid" src="https://i.imgur.com/HKOFQYa.jpeg" alt=""/>
+        </div>
+        <div className="col">
+            <div className="row text-muted">{category}</div>
+            <div className="row">{productName}</div>
+        </div>s
+        <div className="col text-center price">{price}원</div>
+        <div className="col text-end action">
+            <button onClick={handleAddBtnClicked} className="btn btn-small btn-outline-dark">+</button>
+        </div>
+        <div className="col text-end action">
+            <button onClick={handleAddBtnClicked2} className="btn btn-small btn-outline-dark">-</button>
+        </div>
+    </>
+}
